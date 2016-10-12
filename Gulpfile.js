186c483cc90c7b31a62ca04d9afc508f460eb7ab
Olivier Baumann <olivier@ovbm.co>
2016-10-12 13:56:51 +0200

    autosave

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,15 +5,12 @@ var git  = require('gulp-git');
 gulp.task('commit-changes', function () {
   return gulp.src('.')
     .pipe(git.add())
-    .pipe(git.commit('autosave'))
-    .pipe(git.push('origin', 'master', function (err) {
-      if (err) throw err;
-    }));
+    .pipe(git.commit('autosave'));
 });
 
-//Develop task
+//autosave task
 gulp.task('autosave',function() {
-    gulp.watch('',['commit-changes']);
+    gulp.watch('*',['commit-changes']);
 });
 
 gulp.task('default', ['commit-changes']);
